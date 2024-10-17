28426557eb5be0bcbaa36fa00494beaf7bd45047
sind <Sindieiev_Bohdan@proton.me>
2024-10-17 11:49:05 +0300

    version 1.1.a

--- main.py
+++ main.py
@@ -2,42 +2,12 @@ import random
 
 def game():
 
+    number = random.randint(1,100)
     print("Hello! This is Number Guessing Game!")
     print("Rules: You have to guess a random number from 1 to 100")
 
-<<<<<<< Updated upstream
-diff_mes = "1.Easy (10 chances); 2.Normal (5 chances); 3.Hard (3 chances)"
-diff = int(input(f"{diff_mes}\nChoose difficulty: "))
-
-while True:
-    if diff > 3:
-        print("Try again!")
-        diff = int(input(f"{diff_mes}\nChoose difficulty: "))
-    else:
-        if diff == 1:
-            print("You have 10 chances!")
-            break
-        elif diff == 2:
-            print("You have 5 chances!")
-            break
-        elif diff == 3:
-            print("You have 3 chances!")
-            break
-            
-guess_num = 0
-guess_num_max = 0
-if diff == 1:
-    guess_num_max = 10
-elif diff == 2:
-    guess_num_max = 5
-else:
-    guess_num_max = 3
-=======
-    number = random.randint(1,100)
-
     diff_mes = "1.Easy (10 chances); 2.Normal (5 chances); 3.Hard (3 chances)"
     diff = int(input(f"{diff_mes}\nChoose difficulty: "))
->>>>>>> Stashed changes
 
     while True:
         if diff > 3:
