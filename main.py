3781d06a4be48b9e4bb09d4ceb275a77f4741996
sind <Sindieiev_Bohdan@proton.me>
2024-10-17 10:56:48 +0300

    Version 1.1

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
 
-print("Hello! This is Number Guessing Game!")
-print("Rules: You have to guess a random number from 1 to 100")
+def game():
 
-number = random.randint(1,100)
+    print("Hello! This is Number Guessing Game!")
+    print("Rules: You have to guess a random number from 1 to 100")
 
+<<<<<<< Updated upstream
 diff_mes = "1.Easy (10 chances); 2.Normal (5 chances); 3.Hard (3 chances)"
 diff = int(input(f"{diff_mes}\nChoose difficulty: "))
 if diff == 1:
@@ -37,25 +38,66 @@ elif diff == 2:
     guess_num_max = 5
 else:
     guess_num_max = 3
+=======
+    number = random.randint(1,100)
 
-print(number)
-print(guess_num_max)
+    diff_mes = "1.Easy (10 chances); 2.Normal (5 chances); 3.Hard (3 chances)"
+    diff = int(input(f"{diff_mes}\nChoose difficulty: "))
+>>>>>>> Stashed changes
+
+    while True:
+        if diff > 3:
+            print("Try again!")
+            diff = int(input(f"{diff_mes}\nChoose difficulty: "))
+        else:
+            if diff == 1:
+                print("You have 10 chances!")
+                break
+            elif diff == 2:
+                print("You have 5 chances!")
+                break
+            elif diff == 3:
+                print("You have 3 chances!")
+                break
+            
+    guess_num = 0
+    guess_num_max = 0
+    if diff == 1:
+        guess_num_max = 10
+    elif diff == 2:
+        guess_num_max = 5
+    else:
+        guess_num_max = 3
+
+    print(number)
+    print(guess_num_max)
+
+    while True:
+        if guess_num < guess_num_max:
+            guess = int(input("Enter your guess: "))
+            if guess > 100:
+                print("Remember the rules! Enter number between 1 & 100!")
+            else:
+                if guess == number:
+                    print("Congrats!")
+                    break
+                elif guess > number:
+                    guess_num += 1
+                    print(f"Number is smaller than {guess}!")
+                elif guess < number:
+                    guess_num += 1
+                    print(f"Number is bigger than {guess}!")
+        else:
+            print("You lost!")
+            break
 
 while True:
-    if guess_num < guess_num_max:
-        guess = int(input("Enter your guess: "))
-        if guess > 100:
-            print("Remember the rules! Enter number between 1 & 100!")
-        else:
-            if guess == number:
-                print("Congrats!")
-                break
-            elif guess > number:
-                guess_num += 1
-                print(f"Number is smaller than {guess}!")
-            elif guess < number:
-                guess_num += 1
-                print(f"Number is bigger than {guess}!")
-    else:
-        print("You lost!")
+    game()
+    cont = input("Do you wanna play again? [y/n]: \n")
+    if cont.lower() == "y":
+        game()
+    if cont.lower() == "n":
         break
+    else:
+        print("Try again!")
+        cont = input("Do you wanna play again? [y/n]: ")
