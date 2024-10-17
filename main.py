756ccda2c76493aaf53bf5e6b65da8afe69681b6
sind <Sindieiev_Bohdan@proton.me>
2024-10-17 10:29:36 +0300

    Version 1.0.2

--- main.py
+++ main.py
@@ -7,27 +7,21 @@ number = random.randint(1,100)
 
 diff_mes = "1.Easy (10 chances); 2.Normal (5 chances); 3.Hard (3 chances)"
 diff = int(input(f"{diff_mes}\nChoose difficulty: "))
-if diff == 1:
-    print("You have 10 chances!")
-elif diff == 2:
-    print("You have 5 chances!")
-elif diff == 3:
-    print("You have 3 chances!")
-else:
-    while True:
-        if diff > 3:
-            print("Try again!")
-            diff = int(input(f"{diff_mes}\nChoose difficulty: "))
-        else:
-            if diff == 1:
-                print("You have 10 chances!")
-                break
-            elif diff == 2:
-                print("You have 5 chances!")
-                break
-            elif diff == 3:
-                print("You have 3 chances!")
-                break
+
+while True:
+    if diff > 3:
+        print("Try again!")
+        diff = int(input(f"{diff_mes}\nChoose difficulty: "))
+    else:
+        if diff == 1:
+            print("You have 10 chances!")
+            break
+        elif diff == 2:
+            print("You have 5 chances!")
+            break
+        elif diff == 3:
+            print("You have 3 chances!")
+            break
             
 guess_num = 0
 guess_num_max = 0
